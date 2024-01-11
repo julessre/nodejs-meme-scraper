da1a0e073e4a3828c49d96e0ab5056ccb323d2a0
Julia <jules.rechi@gmail.com>
2024-01-11 17:25:40 +0100

    Changed the Array to only the first 10 pictures

--- index.js
+++ index.js
@@ -8,11 +8,13 @@ const getDownloadedImages = async (url) => {
   try {
     const response = await axios.get(url);
     const $ = cheerio.load(response.data);
-    const downloadedImages = [];
-    $('img').each(function (i, elem) {
-      let link = $(elem).attr('src');
-      downloadedImages.push(link);
-    });
+    let downloadedImages = [];
+    $('img')
+      .slice(0, 10)
+      .each(function (i, elem) {
+        let link = $(elem).attr('src');
+        downloadedImages.push(link);
+      });
     console.log(downloadedImages);
   } catch (error) {
     console.error(error);
